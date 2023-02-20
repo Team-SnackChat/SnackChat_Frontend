b3738b975b53050e5a55e7bb31a6e0a90a51799d
nakevin96 <nakevin96@hanyang.ac.kr>
2023-02-20 18:53:04 +0900

    jest configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const { jestPathAlias } = require('./pathAlias');
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    'jest-plugin-context/setup',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  moduleNameMapper: jestPathAlias,
+};
---
+++ pathAlias.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const resolvePath = (relativePath) => path.resolve(__dirname, relativePath);
+
+module.exports = {
+  jestPathAlias: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+  },
+};
